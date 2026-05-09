60f9aed434592a4d2fbc9e414a2c29dcc119e96e
DeadVoxelx <deadvoxelx@gmail.com>
2026-05-09 14:58:33 -0400

    Update PackedGlassTile.cpp

--- Minecraft.World/PackedGlassTile.cpp
+++ Minecraft.World/PackedGlassTile.cpp
@@ -15,3 +15,13 @@ shared_ptr<ItemInstance> PackedGlassTile::getSilkTouchItemInstance(int data)
 {
 	return shared_ptr<ItemInstance>(new ItemInstance(Tile::blueIce));
 }
+
+int PackedGlassTile::getColor(LevelSource *level, int x, int y, int z)
+{
+	return (x * x * 3187961 + x * 987243 + y * y * 43297126 + y * 987121 + z * z * 927469861 + z * 1861) & 0xffffff;
+}
+
+int PackedGlassTile::getColor(LevelSource *level, int x, int y, int z, int data)
+{
+	return (x * x * 3187961 + x * 987243 + y * y * 43297126 + y * 987121 + z * z * 927469861 + z * 1861) & 0xffffff;
+}
