3a5d304e6e0702b69df8b342b0754d43104901d1
DeadVoxelx <deadvoxelx@gmail.com>
2026-05-29 23:54:13 -0400

    Update CowRenderer.cpp

--- Minecraft.Client/CowRenderer.cpp
+++ Minecraft.Client/CowRenderer.cpp
@@ -1,7 +1,12 @@
 #include "stdafx.h"
 #include "CowRenderer.h"
+#include "..\Minecraft.World\net.minecraft.world.entity.monster.h"
+#include "..\Minecraft.World\net.minecraft.world.entity.animal.h"
 
 ResourceLocation CowRenderer::COW_LOCATION = ResourceLocation(TN_MOB_COW);
+ResourceLocation CowRenderer::COW_BROWN_LOCATION = ResourceLocation(TN_MOB_COW_BROWN);
+ResourceLocation CowRenderer::COW_BLACK_LOCATION = ResourceLocation(TN_MOB_COW_BLACK);
+ResourceLocation CowRenderer::COW_INVERT_LOCATION = ResourceLocation(TN_MOB_COW_INVERT);
 
 CowRenderer::CowRenderer(Model *model, float shadow) : MobRenderer(model, shadow)
 {
@@ -14,5 +19,22 @@ void CowRenderer::render(shared_ptr<Entity> _mob, double x, double y, double z, 
 
 ResourceLocation *CowRenderer::getTextureLocation(shared_ptr<Entity> mob)
 {
-    return &COW_LOCATION;
+    shared_ptr<Cow> cow = dynamic_pointer_cast<Cow>(mob);
+
+    if (cow->getCowType() == Cow::TYPE_DEFAULT)
+	{
+        return &COW_LOCATION;
+    }
+    if (cow->getCowType() == Cow::TYPE_BROWN)
+	{
+        return &COW_BROWN_LOCATION;
+    }
+    if (cow->getCowType() == Cow::TYPE_BLACK)
+	{
+        return &COW_BLACK_LOCATION;
+    }
+    if (cow->getCowType() == Cow::TYPE_INVERT)
+	{
+        return &COW_INVERT_LOCATION;
+    }
 }
