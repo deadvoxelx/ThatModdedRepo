b9787313680e0fa0c8d2783296dc6dc9e5d47cf3
DeadVoxelx <deadvoxelx@gmail.com>
2026-06-01 09:19:10 -0400

    Update NetherLeavesTile.cpp

--- Minecraft.World/NetherLeavesTile.cpp
+++ Minecraft.World/NetherLeavesTile.cpp
@@ -24,41 +24,116 @@ int NetherLeavesTile::getResourceCount(Random *random)
 
 shared_ptr<ItemInstance> NetherLeavesTile::getSilkTouchItemInstance(int data)
 {
+	if (id == Tile::skyrootLeaves_Id) return shared_ptr<ItemInstance>(new ItemInstance(Tile::skyrootLeaves));
+	if (id == Tile::goldenOakLeaves_Id) return shared_ptr<ItemInstance>(new ItemInstance(Tile::goldenOakLeaves));
 	return shared_ptr<ItemInstance>(new ItemInstance(Tile::netherLeaves));
 }
 
 void NetherLeavesTile::spawnResources(Level *level, int x, int y, int z, int data, float odds, int playerBonusLevel)
 {
-	if (!level->isClientSide)
+	if (id == Tile::netherLeaves_Id)
 	{
-		int chance = 5;
-
-		chance = 5;
-		if (playerBonusLevel > 0)
+		if (!level->isClientSide)
 		{
-			chance -= 1 << playerBonusLevel;
-			if (chance < 2)
+			int chance = 5;
+
+			chance = 5;
+			if (playerBonusLevel > 0)
 			{
-				chance = 2;
+				chance -= 1 << playerBonusLevel;
+				if (chance < 2)
+				{
+					chance = 2;
+				}
+			}
+			if (level->random->nextInt(chance) == 0)
+			{
+				popResource(level, x, y, z, std::make_shared<ItemInstance>(Tile::netherSapling_Id, 1, 0));
+			}
+
+			chance = 5;
+			if (playerBonusLevel > 0)
+			{
+				chance -= 1 << playerBonusLevel;
+				if (chance < 2)
+				{
+					chance = 2;
+				}
+			}
+			if (level->random->nextInt(chance) == 0)
+			{
+				popResource(level, x, y, z, std::make_shared<ItemInstance>(Item::stick_Id, 1, 0));
 			}
 		}
-		if (level->random->nextInt(chance) == 0)
+	}
+	else if (id == Tile::skyrootLeaves_Id)
+	{
+		if (!level->isClientSide)
 		{
-			popResource(level, x, y, z, std::make_shared<ItemInstance>(Tile::netherSapling_Id, 1, 0));
-		}
+			int chance = 5;
 
-		chance = 5;
-		if (playerBonusLevel > 0)
-		{
-			chance -= 1 << playerBonusLevel;
-			if (chance < 2)
+			chance = 5;
+			if (playerBonusLevel > 0)
 			{
-				chance = 2;
+				chance -= 1 << playerBonusLevel;
+				if (chance < 2)
+				{
+					chance = 2;
+				}
 			}
+			if (level->random->nextInt(chance) == 0)
+			{
+				popResource(level, x, y, z, std::make_shared<ItemInstance>(Tile::netherSapling_Id, 1, 0));
+			}
+
+			/*chance = 5;
+			if (playerBonusLevel > 0)
+			{
+				chance -= 1 << playerBonusLevel;
+				if (chance < 2)
+				{
+					chance = 2;
+				}
+			}
+			if (level->random->nextInt(chance) == 0)
+			{
+				popResource(level, x, y, z, std::make_shared<ItemInstance>(Item::stick_Id, 1, 0));
+			}*/
 		}
-		if (level->random->nextInt(chance) == 0)
+	}
+	else if (id == Tile::goldenOakLeaves_Id)
+	{
+		if (!level->isClientSide)
 		{
-			popResource(level, x, y, z, std::make_shared<ItemInstance>(Item::stick_Id, 1, 0));
+			int chance = 5;
+
+			chance = 5;
+			if (playerBonusLevel > 0)
+			{
+				chance -= 1 << playerBonusLevel;
+				if (chance < 2)
+				{
+					chance = 2;
+				}
+			}
+			if (level->random->nextInt(chance) == 0)
+			{
+				popResource(level, x, y, z, std::make_shared<ItemInstance>(Tile::netherSapling_Id, 1, 0));
+			}
+
+			/*chance = 5;
+			if (playerBonusLevel > 0)
+			{
+				chance -= 1 << playerBonusLevel;
+				if (chance < 2)
+				{
+					chance = 2;
+				}
+			}
+			if (level->random->nextInt(chance) == 0)
+			{
+				popResource(level, x, y, z, std::make_shared<ItemInstance>(Item::stick_Id, 1, 0));
+			}*/
 		}
 	}
 }
