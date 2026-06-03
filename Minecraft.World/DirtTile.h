5fdbc8956bf906a9518b0098d27463cb18b67ec4
DeadVoxelx <deadvoxelx@gmail.com>
2026-06-03 10:12:20 -0400

    Update DirtTile.h

--- Minecraft.World/DirtTile.h
+++ Minecraft.World/DirtTile.h
@@ -5,19 +5,19 @@ class Random;
 
 class DirtTile : public Tile
 {
-public:
-	static const int TYPE_DEFAULT = 0;
-	static const int TYPE_AETHER = 1;
+//public:
+	//static const int TYPE_DEFAULT = 0;
+	//static const int TYPE_AETHER = 1;
 
-	static const int DIRT_NAMES_LENGTH = 2;
+	//static const int DIRT_NAMES_LENGTH = 2;
 
-	static const unsigned int DIRT_NAMES[DIRT_NAMES_LENGTH];
-	static const wstring TEXTURE_NAMES[];
-private:
-	Icon** icons;
+	//static const unsigned int DIRT_NAMES[DIRT_NAMES_LENGTH];
+	//static const wstring TEXTURE_NAMES[];
+//private:
+	//Icon** icons;
 public:
 	DirtTile(int id);
-	virtual Icon* getTexture(int face, int data);
-	virtual int getSpawnResourcesAuxValue(int data);
-	void registerIcons(IconRegister* iconRegister);
+	//virtual Icon* getTexture(int face, int data);
+	//virtual int getSpawnResourcesAuxValue(int data);
+	//void registerIcons(IconRegister* iconRegister);
 };
