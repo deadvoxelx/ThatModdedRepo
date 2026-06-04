3b48270a4a6f9f83e982c82abaab081d9217d03d
DeadVoxelx <deadvoxelx@gmail.com>
2026-06-03 21:47:50 -0400

    Update MinecraftServer.cpp

--- Minecraft.Client/MinecraftServer.cpp
+++ Minecraft.Client/MinecraftServer.cpp
@@ -888,7 +888,7 @@ bool MinecraftServer::loadLevel(LevelStorageSource *storageSource, const wstring
 	ProgressRenderer *mcprogress = Minecraft::GetInstance()->progressRenderer;
 
 	// 4J TODO - free levels here if there are already some?
-	levels = ServerLevelArray(4);
+	levels = ServerLevelArray(5); //
 
 	int gameTypeId = GetDedicatedServerInt(settings, L"gamemode", app.GetGameHostOption(eGameHostOption_GameType));//LevelSettings::GAMETYPE_SURVIVAL);
 	GameType *gameType = LevelSettings::validateGameType(gameTypeId);
@@ -966,6 +966,7 @@ bool MinecraftServer::loadLevel(LevelStorageSource *storageSource, const wstring
 		if (i == 1) dimension = -1;
 		if (i == 2) dimension = 1;
 		if (i == 3) dimension = 2;
+		if (i == 4) dimension = 3;
 		if (i == 0)
 		{
 			levels[i] = new ServerLevel(this, storage, name, dimension, levelSettings);
@@ -1240,6 +1241,13 @@ bool MinecraftServer::loadLevel(LevelStorageSource *storageSource, const wstring
 
 	if( s_bServerHalted || !g_NetworkManager.IsInSession() ) return false;
 
+	if( levels[4]->isNew )
+	{
+		levels[4]->save(true, mcprogress);
+	}
+
+	if( s_bServerHalted || !g_NetworkManager.IsInSession() ) return false;
+
 	// 4J - added - immediately save newly created level, like single player game
 	// 4J Stu - We also want to immediately save the tutorial
 	if ( levels[0]->isNew )
@@ -1252,7 +1260,7 @@ bool MinecraftServer::loadLevel(LevelStorageSource *storageSource, const wstring
 
 	if( s_bServerHalted || !g_NetworkManager.IsInSession() ) return false;
 
-	if( levels[0]->isNew || levels[1]->isNew || levels[2]->isNew || levels[3]->isNew )
+	if( levels[0]->isNew || levels[1]->isNew || levels[2]->isNew || levels[3]->isNew /*|| levels[4]->isNew*/)
 	{
 #ifndef _WINDOWS64
 		// On Windows64 we skip the automatic initial save so that choosing
@@ -2298,6 +2306,7 @@ ServerLevel *MinecraftServer::getLevel(int dimension)
 	if (dimension == -1) return levels[1];
 	else if (dimension == 1) return levels[2];
 	else if (dimension == 2) return levels[3];
+	else if (dimension == 3) return levels[4];
 	else return levels[0];
 }
 
@@ -2307,6 +2316,7 @@ void MinecraftServer::setLevel(int dimension, ServerLevel *level)
 	if (dimension == -1) levels[1] = level;
 	else if (dimension == 1) levels[2] = level;
 	else if (dimension == 2) levels[3] = level;
+	else if (dimension == 3) levels[4] = level;
 	else levels[0] = level;
 }
 
