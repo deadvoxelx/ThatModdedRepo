b8b0ec6972d8f7ad5ea3c549acaa4a1d2a77f781
DeadVoxelx <deadvoxelx@gmail.com>
2026-06-03 21:45:51 -0400

    Update LevelStorage.cpp

--- Minecraft.World/LevelStorage.cpp
+++ Minecraft.World/LevelStorage.cpp
@@ -5,3 +5,4 @@
 const wstring LevelStorage::NETHER_FOLDER = L"DIM-1";
 const wstring LevelStorage::ENDER_FOLDER = L"DIM1/";
 const wstring LevelStorage::OUTEREND_FOLDER = L"DIM2/";
+const wstring LevelStorage::AETHER_FOLDER = L"DIM3/";
