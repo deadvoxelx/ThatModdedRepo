c52fd183296df645855026f8919aeecb57bfe746
DeadVoxelx <deadvoxelx@gmail.com>
2026-06-09 19:32:50 -0400

    Update WeaponItem.cpp

--- Minecraft.World/WeaponItem.cpp
+++ Minecraft.World/WeaponItem.cpp
@@ -28,19 +28,15 @@ float WeaponItem::getDestroySpeed(shared_ptr<ItemInstance> itemInstance, Tile *t
 		return 15;
 	}
 
-	if (tile->id == Tile::gravel_Id)
+	if (tile->id == Tile::gravel_Id || tile->id == Tile::lavaRock_Id || tile->id == Tile::quicksoil_Id)
 	{
 		return 1.5f;
 	}
 
-	if (tile->id == Tile::lavaRock_Id)
-	{
-		return 1.5f;
-	}
 	// this change modifies which tiles the swords can destroy in creative
 	// mode (>1 == yes)
 	Material *material = tile->material;
-	if (material == Material::plant || material == Material::replaceable_plant || material == Material::coral || material == Material::leaves || material == Material::vegetable || material == Material::dirt)
+	if (material == Material::plant || material == Material::replaceable_plant || material == Material::coral || material == Material::leaves || material == Material::vegetable || material == Material::dirt || material == Material::grass)
 	{
 		return 1.5f;
 	}
