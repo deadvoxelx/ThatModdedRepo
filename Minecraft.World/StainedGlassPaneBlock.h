47b8e37a1ff00dff0dcc77fe62194a43157826f5
DeadVoxelx <deadvoxelx@gmail.com>
2026-06-10 19:05:07 -0400

    Update StainedGlassPaneBlock.h

--- Minecraft.World/StainedGlassPaneBlock.h
+++ Minecraft.World/StainedGlassPaneBlock.h
@@ -5,19 +5,42 @@
 class StainedGlassPaneBlock : public ThinFenceTile
 {
 	friend class ChunkRebuildData;
+public:
+	static const int BLACK = 0;
+	static const int RED = 1;
+	static const int GREEN = 2;
+	static const int BROWN = 3;
+	static const int BLUE = 4;
+	static const int PURPLE = 5;
+	static const int CYAN = 6;
+	static const int LIGHTGRAY = 7;
+	static const int GRAY = 8;
+	static const int PINK = 9;
+	static const int LIME = 10;
+	static const int YELLOW = 11;
+	static const int LIGHTBLUE = 12;
+	static const int MAGENTA = 13;
+	static const int ORANGE = 14;
+	static const int WHITE = 15;
+
+	static const int STAINED_GLASS_NAMES_LENGTH = 16;
+
+	static const unsigned int STAINED_GLASS_NAMES[STAINED_GLASS_NAMES_LENGTH];
+
 private:
-	static const int ICONS_COUNT = 16;
-	static Icon *ICONS[ICONS_COUNT];
-	static Icon *EDGE_ICONS[ICONS_COUNT];
+	Icon *iconSide;
 
 public:
 	StainedGlassPaneBlock(int id);
 
-	Icon *getIconTexture(int face, int data);
-	Icon *getEdgeTexture(int data);
-	Icon *getTexture(int face, int data);
 	int getSpawnResourcesAuxValue(int data);
 	static int getItemAuxValueForBlockData(int data);
 	int getRenderLayer();
 	void registerIcons(IconRegister *iconRegister);
+	Icon *getEdgeTexture(int data);
+
+	virtual int getColor() const;
+	virtual int getColor(int auxData);
+	virtual int getColor(LevelSource *level, int x, int y, int z);
+	virtual int getColor(LevelSource *level, int x, int y, int z, int data);
 };
