2e007ffb991715769e5172991fac49eae54db4af
DeadVoxelx <deadvoxelx@gmail.com>
2026-06-10 07:08:12 -0400

    Update QuicksoilTile.h

--- Minecraft.World/QuicksoilTile.h
+++ Minecraft.World/QuicksoilTile.h
@@ -11,11 +11,21 @@ public:
 	static const int TYPE_NATURAL = 0;
 	static const int TYPE_UNNATURAL = 1;
 
+	static const int QUICKSOIL_NAMES_LENGTH = 2;
+
+	static const unsigned int QUICKSOIL_NAMES[QUICKSOIL_NAMES_LENGTH];
+	static const wstring TEXTURE_NAMES[];
+private:
+	Icon** icons;
+
 protected:
 	QuicksoilTile(int id);
 	virtual shared_ptr<ItemInstance> getSilkTouchItemInstance(int data);
 
 public:
 	virtual void stepOn(Level *level, int x, int y, int z, shared_ptr<Entity> entity);
-	virtual void playerDestroy(Level *level, shared_ptr<Player> player, int x, int y, int z, int data);
+	virtual int getResource(int data, Random *random, int playerBonusLevel);
+	virtual void spawnResources(Level *level, int x, int y, int z, int data, float odds, int playerBonusLevel);
+	virtual Icon* getTexture(int face, int data);
+	void registerIcons(IconRegister* iconRegister);
 };
