dab9295976e9852d664d32e49a5edbe0f6269df3
DeadVoxelx <deadvoxelx@gmail.com>
2026-06-10 05:26:01 -0400

    Update QuicksoilTile.h

--- Minecraft.World/QuicksoilTile.h
+++ Minecraft.World/QuicksoilTile.h
@@ -7,9 +7,15 @@ class QuicksoilTile : public Tile
 {
 	friend class Tile;
 
+public:
+	static const int TYPE_NATURAL = 0;
+	static const int TYPE_UNNATURAL = 1;
+
 protected:
 	QuicksoilTile(int id);
+	virtual shared_ptr<ItemInstance> getSilkTouchItemInstance(int data);
 
 public:
 	virtual void stepOn(Level *level, int x, int y, int z, shared_ptr<Entity> entity);
+	virtual void playerDestroy(Level *level, shared_ptr<Player> player, int x, int y, int z, int data);
 };
