f808528eeb20b57226169219b39ecad48d6cfeaf
DeadVoxelx <deadvoxelx@gmail.com>
2026-06-10 05:26:35 -0400

    Update QuicksoilTile.cpp

--- Minecraft.World/QuicksoilTile.cpp
+++ Minecraft.World/QuicksoilTile.cpp
@@ -3,6 +3,7 @@
 #include "net.minecraft.world.entity.h"
 #include "net.minecraft.world.entity.player.h"
 #include "net.minecraft.world.level.h"
+#include "net.minecraft.world.item.h"
 #include "Mth.h"
 
 QuicksoilTile::QuicksoilTile(int id) : Tile(id, Material::sand)
@@ -14,4 +15,28 @@ void QuicksoilTile::stepOn(Level *level, int x, int y, int z, shared_ptr<Entity>
 {
 	entity->xd*=1.6;
     entity->zd*=1.6;
+}
+
+void QuicksoilTile::playerDestroy(Level *level, shared_ptr<Player> player, int x, int y, int z, int data)
+{
+	int d = data;
+	if (d == TYPE_NATURAL)
+	{
+		if (!level->isClientSide)
+		{
+			popResource(level, x, y, z, shared_ptr<ItemInstance>(new ItemInstance(Tile::quicksoil_Id, 2, 1)));
+		}
+	}
+	else if (d == TYPE_UNNATURAL)
+	{
+		if (!level->isClientSide)
+		{
+			popResource(level, x, y, z, shared_ptr<ItemInstance>(new ItemInstance(Tile::quicksoil_Id, 1, 1)));
+		}
+	}
+}
+
+shared_ptr<ItemInstance> QuicksoilTile::getSilkTouchItemInstance(int data)
+{
+	return shared_ptr<ItemInstance>(new ItemInstance(Tile::quicksoil, 1, 1));
 }
