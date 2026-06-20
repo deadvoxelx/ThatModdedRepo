8848b3fb4ce88d45eb8b62255662ff14ef73477a
DeadVoxelx <deadvoxelx@gmail.com>
2026-06-20 04:08:42 -0400

    Update EndTowerFeature.h

--- Minecraft.World/EndTowerFeature.h
+++ Minecraft.World/EndTowerFeature.h
@@ -12,6 +12,6 @@ public:
     virtual bool place(Level *level, Random *random, int x, int y, int z);
 
 private:
-	static const int TREASURE_ITEMS_COUNT = 12;
+	static const int TREASURE_ITEMS_COUNT = 19;
 	static WeighedTreasure *endTowerTreasure[TREASURE_ITEMS_COUNT];
 };
