2e080447b0cdac83adaf66ec27b960dbf4de97dd
DeadVoxelx <deadvoxelx@gmail.com>
2026-06-21 18:24:06 -0400

    Update AerwhaleModel.h

--- Minecraft.Client/AerwhaleModel.h
+++ Minecraft.Client/AerwhaleModel.h
@@ -4,11 +4,14 @@
 class AerwhaleModel : public Model
 {
 public:
-	ModelPart *body, *body2, *body3, *fin1, *fin2, *fin3, *fin4;
+	ModelPart *head, *bottomPartHead, *middleBody, *bottomPartMiddlebody, *frontBody, *backBody, *rightFin, *leftFin, *middleFin, *backfinRight, *backfinLeft;
 
 	void _init(float g);
     AerwhaleModel();
     AerwhaleModel(float g);
     virtual void render(shared_ptr<Entity> entity, float time, float r, float bob, float yRot, float xRot, float scale, bool usecompiled);
     virtual void setupAnim(float time, float r, float bob, float yRot, float xRot, float scale, shared_ptr<Entity> entity, unsigned int uiBitmaskOverrideAnim=0);
+
+private:
+	void setRotation(ModelPart *model, float x, float y, float z);
 };
