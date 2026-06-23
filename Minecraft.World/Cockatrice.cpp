a7dbad4c2bf99bda7022f18f098d1360959bd8b5
DeadVoxelx <deadvoxelx@gmail.com>
2026-06-23 06:46:15 -0400

    Update Cockatrice.cpp

--- Minecraft.World/Cockatrice.cpp
+++ Minecraft.World/Cockatrice.cpp
@@ -19,6 +19,14 @@
 #include "..\Minecraft.Client\Minecraft.h"
 #include "..\Minecraft.Client\Textures.h"
 
+void Cockatrice::_init()
+{
+	flap = 0;
+	flapSpeed = 0;
+	flapping = 1;
+	oFlapSpeed = oFlap = 0.0f;
+}
+
 Cockatrice::Cockatrice(Level *level) : Monster( level )
 {
 	this->defineSynchedData();
@@ -55,15 +63,31 @@ void Cockatrice::aiStep()
 {
 	Monster::aiStep();
 
+	oFlap = flap;
+	oFlapSpeed = flapSpeed;
+
+	flapSpeed += (onGround ? -1 : 4) * 0.3f;
+	if (flapSpeed < 0) flapSpeed = 0;
+	if (flapSpeed > 0.75) flapSpeed = 0.75;
+
+	if (!onGround && flapping < 1) flapping = 1;
+	flapping *= 0.9;
+
 	if (!onGround && yd < 0) 
 	{
 		yd *= 0.6;
 	}
+
+	flap += flapping * 2;
 }
 
 void Cockatrice::newServerAiStep()
 {
 	Monster::newServerAiStep();
+}
+
+void Cockatrice::causeFallDamage(float distance) 
+{
 }
 
 void Cockatrice::performRangedAttack(shared_ptr<LivingEntity> target, float power)
