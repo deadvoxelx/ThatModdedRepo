4da764b3353aee0ef7858c8da360db61d69a16af
DeadVoxelx <deadvoxelx@gmail.com>
2026-06-23 06:45:44 -0400

    Update Cockatrice.h

--- Minecraft.World/Cockatrice.h
+++ Minecraft.World/Cockatrice.h
@@ -9,9 +9,17 @@ class RangedAttackGoal;
 
 class Cockatrice : public Monster, public RangedAttackMob
 {
+private:	
+	void _init();
+
 public:
 	eINSTANCEOF GetType() { return eTYPE_COCKATRICE; }
 	static Entity *create(Level *level) { return new Cockatrice(level); }
+
+	float flap;
+    float flapSpeed;
+    float oFlapSpeed, oFlap;
+    float flapping;
 
 	Cockatrice(Level *level);
 	virtual bool useNewAi();
@@ -23,4 +31,5 @@ public:
 protected:
 	virtual void registerAttributes();
 	virtual void newServerAiStep();
+	virtual void causeFallDamage(float distance);
 };
