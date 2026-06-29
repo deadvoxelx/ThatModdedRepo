fcb5c36e414f1a0cce7f70c3c3d3fd11d5f7a16c
DeadVoxelx <deadvoxelx@gmail.com>
2026-06-29 06:31:13 -0400

    Update PortalForcer.h

--- Minecraft.World/PortalForcer.h
+++ Minecraft.World/PortalForcer.h
@@ -4,18 +4,27 @@ class Random;
 
 class PortalForcer
 {
+public:
+	class PortalPosition : public Pos
+	{
+	public:
+		int64_t lastUsed;
+
+		PortalPosition(int x, int y, int z, int64_t time);
+	};
+
 private:
+	ServerLevel *level;
 	Random *random;
+	unordered_map<int64_t, PortalPosition *> cachedPortals;
+	vector<int64_t> cachedPortalKeys;
 
 public:
-	// 4J Stu Added - Java has no ctor, but we need to initialise random
-	PortalForcer();
+	PortalForcer(ServerLevel *level);
+	~PortalForcer();
 
 	void force(Level *level, shared_ptr<Entity> e, int lastDimension = -1);
-
-public:
 	bool findPortal(Level *level, shared_ptr<Entity> e, int lastDimension = -1);
-
-public:
 	bool createPortal(Level *level, shared_ptr<Entity> e, int lastDimension = -1);
+	void tick(int64_t time);
 };
