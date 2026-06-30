4ff8bf8fa9a8c8b141ca8044f5e2b079ff30f6c5
DeadVoxelx <deadvoxelx@gmail.com>
2026-06-30 12:49:02 -0400

    Update GlassTile.cpp

--- Minecraft.World/GlassTile.cpp
+++ Minecraft.World/GlassTile.cpp
@@ -1,33 +1,63 @@
-#pragma once
-#include "HalfTransparentTile.h"
+#include "stdafx.h"
+#include "GlassTile.h"
+#include "net.minecraft.world.h"
+#include "net.minecraft.h"
 
-class Random;
-
-class GlassTile : public HalfTransparentTile
-{
-public:
-	static const int TYPE_DEFAULT = 0;
-	static const int TYPE_SOUL = 1;
-	static const int TYPE_QUICKSOIL = 2;
-
-	static const int GLASS_NAMES_LENGTH = 3;
-
-	static const unsigned int GLASS_NAMES[GLASS_NAMES_LENGTH];
-	static const wstring TEXTURE_NAMES[];
-
-private:
-	Icon** icons;
-
-public:
-	using HalfTransparentTile::isSolidRender;
-
-	GlassTile(int id, Material *material);
-	virtual int getResourceCount(Random *random);
-	virtual int getRenderLayer();
-	virtual bool isSolidRender();
-	virtual bool isCubeShaped();
-	virtual bool isSilkTouchable();
-
-	virtual Icon* getTexture(int face, int data);
-	void registerIcons(IconRegister* iconRegister);
+const unsigned int GlassTile::GLASS_NAMES[GLASS_NAMES_LENGTH] = {
+	IDS_TILE_GLASS,
+	IDS_TILE_GLASS_SOUL,
+	IDS_TILE_GLASS_QUICKSOIL,
 };
+
+const wstring GlassTile::TEXTURE_NAMES[] = {
+	L"glass", L"glass", L"quicksoilGlass"
+};
+
+GlassTile::GlassTile(int id, Material *material) : HalfTransparentTile(id, L"glass", material, false)
+{
+	icons = nullptr;
+}
+
+int GlassTile::getResourceCount(Random *random)
+{
+	return 0;
+}
+
+int GlassTile::getRenderLayer()
+{
+	return 1;
+}
+
+bool GlassTile::isSolidRender()
+{
+	return false;
+}
+
+bool GlassTile::isCubeShaped()
+{
+	return false;
+}
+
+bool GlassTile::isSilkTouchable()
+{
+	return true;
+}
+
+Icon* GlassTile::getTexture(int face, int data)
+{
+	if (data < 0 || data >= GLASS_NAMES_LENGTH)
+	{
+		data = 0;
+	}
+	return icons[data];
+}
+
+void GlassTile::registerIcons(IconRegister* iconRegister)
+{
+	icons = new Icon * [GLASS_NAMES_LENGTH];
+
+	for (int i = 0; i < GLASS_NAMES_LENGTH; i++)
+	{
+		icons[i] = iconRegister->registerIcon(TEXTURE_NAMES[i]);
+	}
+}
