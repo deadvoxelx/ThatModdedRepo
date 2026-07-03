b8e0c3837ac2005ee24a2cf651df111c12cd2fed
DeadVoxelx <deadvoxelx@gmail.com>
2026-07-03 15:55:15 -0400

    Update BirchFeature.h

--- Minecraft.World/BirchFeature.h
+++ Minecraft.World/BirchFeature.h
@@ -7,5 +7,6 @@ class BirchFeature : public Feature
 {
 public:
 	BirchFeature(bool doUpdate);
+	bool branch(Level *level, Random *random, int x, int y, int z, int slant);
     virtual bool place(Level *level, Random *random, int x, int y, int z);
 };
