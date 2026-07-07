92338ef630e31ebf2a09a83d63a7251a7e822abf
DeadVoxelx <deadvoxelx@gmail.com>
2026-07-06 21:08:09 -0400

    Update LeafTile.h

--- Minecraft.World/LeafTile.h
+++ Minecraft.World/LeafTile.h
@@ -1,78 +1,54 @@
 #pragma once
 #include "TransparentTile.h"
 
-class Random;
-class ChunkRebuildData;
-
 class LeafTile : public TransparentTile
 {
 	friend class Tile;
-	friend class ChunkRebuildData;
 public:
-	static const wstring TEXTURES[3][5];
-	static const int REQUIRED_WOOD_RANGE = 4;
+	static const int REQUIRED_WOOD_RANGE = 8;
+    //static const int UPDATE_LEAF_BIT = 8;
+	//static const int PERSISTENT_LEAF_BIT = 4;
 
-    static const int UPDATE_LEAF_BIT = 8;
-	static const int PERSISTENT_LEAF_BIT = 4;	// player-placed
-
-    static const int NORMAL_LEAF = 0;
-    static const int EVERGREEN_LEAF = 1;
-    static const int BIRCH_LEAF = 2;
+	static const int NORMAL_LEAF = 0;
+	static const int EVERGREEN_LEAF = 1;
+	static const int BIRCH_LEAF = 2;
 	static const int JUNGLE_LEAF = 3;
 	static const int PURPLE_LEAF = 4;
 
 	static const int LEAF_NAMES_LENGTH = 5;
 
 	static const unsigned int LEAF_NAMES[LEAF_NAMES_LENGTH];
+	static const wstring TEXTURE_NAMES[];
+
+	int *checkBuffer;
+
 private:
-	static const int LEAF_TYPE_MASK = 4;
-
-    // pppppppppp ppppppppppp pppppppppp ppppppp
-    // ssssssssss sssssssssss s
-
-    int fancyTextureSet;
-	Icon *icons[3][5];
+	Icon** icons;
 
 protected:
 	LeafTile(int id);
 	virtual ~LeafTile();
-public:
-	virtual int getColor() const;
-	virtual int getColor(int data);
-
-    virtual int getColor(LevelSource *level, int x, int y, int z);
-	virtual int getColor(LevelSource *level, int x, int y, int z, int data);	// 4J added
-    virtual void onRemove(Level *level, int x, int y, int z, int id, int data);
-    int *checkBuffer;
-
-    virtual void tick(Level *level, int x, int y, int z, Random *random);
-	virtual void animateTick(Level *level, int x, int y, int z, Random *random);
-private:
-	void die(Level *level, int x, int y, int z);
-public:
-	virtual int getResourceCount(Random *random);
-    virtual int getResource(int data, Random *random, int playerBonusLevel);
-
-	// 4J DCR: Brought forward from 1.2
-	virtual void spawnResources(Level *level, int x, int y, int z, int data, float odds, int playerBonusLevel);
-
-	virtual void playerDestroy(Level *level, shared_ptr<Player> player, int x, int y, int z, int data);
-protected:
-	virtual int getSpawnResourcesAuxValue(int data);
-public:
-	virtual bool isSolidRender(bool isServerLevel = false);
-    virtual Icon *getTexture(int face, int data);
-    void setFancy(bool fancyGraphics);
-
-protected:
 	virtual shared_ptr<ItemInstance> getSilkTouchItemInstance(int data);
 
 public:
-    virtual void stepOn(Level *level, int x, int y, int z, shared_ptr<Entity> entity);
-	
-	// 4J Added so we can check before we try to add a tile to the tick list if it's actually going to do seomthing
-	virtual bool shouldTileTick(Level *level, int x,int y,int z);
+	virtual int getResource(int data, Random *random, int playerBonusLevel);
+	virtual int getResourceCountForLootBonus(int bonusLevel, Random *random);
+	virtual int getResourceCount(Random *random);
+	virtual void spawnResources(Level *level, int x, int y, int z, int data, float odds, int playerBonusLevel);
 
-	virtual unsigned int getDescriptionId(int iData = -1);
-	void registerIcons(IconRegister *iconRegister);
+	virtual Icon* getTexture(int face, int data);
+	void registerIcons(IconRegister* iconRegister);
+	virtual bool isSolidRender(bool isServerLevel = false);
+	
+	virtual int getColor() const;
+	virtual int getColor(int data);
+	virtual int getColor(LevelSource *level, int x, int y, int z);
+	virtual int getColor(LevelSource *level, int x, int y, int z, int data);
+
+	virtual void animateTick(Level *level, int x, int y, int z, Random *random);
+	virtual void onRemove(Level *level, int x, int y, int z, int id, int data);
+	virtual void tick(Level *level, int x, int y, int z, Random *random);
+
+private:
+	void die(Level *level, int x, int y, int z);
 };
