58fd569044f67c736e23a8d84b2c96c40c080f8c
DeadVoxelx <deadvoxelx@gmail.com>
2026-07-18 13:37:24 -0400

    Update McRegionLevelStorageSource.cpp

--- Minecraft.World/McRegionLevelStorageSource.cpp
+++ Minecraft.World/McRegionLevelStorageSource.cpp
@@ -126,6 +126,8 @@ bool McRegionLevelStorageSource::convertLevel(ConsoleSaveFile *saveFile, const w
     ArrayList<File> outerEndBaseFolders = new ArrayList<File>();
 	ArrayList<ChunkFile> aetherRegions = new ArrayList<ChunkFile>();
     ArrayList<File> aetherBaseFolders = new ArrayList<File>();
+	ArrayList<ChunkFile> nurealmRegions = new ArrayList<ChunkFile>();
+    ArrayList<File> nurealmBaseFolders = new ArrayList<File>();
 
 	//File baseFolder = File(baseDir, levelId);
 	//File netherFolder = File(baseFolder, LevelStorage::HELL_FOLDER);
@@ -158,7 +160,12 @@ bool McRegionLevelStorageSource::convertLevel(ConsoleSaveFile *saveFile, const w
         addRegions(aetherFolder, aetherRegions, aetherBaseFolders);
     }
 
-	int totalCount = normalRegions->size() + netherRegions->size() + enderRegions.size() + outerEndRegions.size() + aetherRegions.size() + normalBaseFolders->size() + netherBaseFolders->size() + enderBaseFolders.size() + outerEndBaseFolders.size() + aetherBaseFolders.size();
+	if (nurealmFolder.exists())
+	{
+		addRegions(nurealmFolder, nurealmRegions, nurealmBaseFolders);
+	}
+
+	int totalCount = normalRegions->size() + netherRegions->size() + enderRegions.size() + outerEndRegions.size() + aetherRegions.size() + nurealmRegions.size() + normalBaseFolders->size() + netherBaseFolders->size() + enderBaseFolders.size() + outerEndBaseFolders.size() + aetherBaseFolders.size() + nurealmBaseFolders.size();
 
 	// System.out.println("Total conversion count is " + totalCount); 4J Jev, TODO
 
@@ -172,6 +179,8 @@ bool McRegionLevelStorageSource::convertLevel(ConsoleSaveFile *saveFile, const w
 	convertRegions(outerEndFolder, outerEndRegions, normalRegions.size() + netherRegions.size() + enderRegions.size(), totalCount, progress);
 	// convert aether world
 	convertRegions(aetherFolder, aetherRegions, normalRegions.size() + netherRegions.size() + enderRegions.size() + outerEndRegions.size(), totalCount, progress);
+	// convert nurealm world
+	convertRegions(nurealmFolder, nurealmRegions, normalRegions.size() + netherRegions.size() + enderRegions.size() + outerEndRegions.size() + aetherRegions.size(), totalCount, progress);
 
 	LevelData *levelData = getDataTagFor(levelId);
 	levelData->setVersion(McRegionLevelStorage::MCREGION_VERSION_ID);
