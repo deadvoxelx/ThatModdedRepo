290473c7df6491bac828817df72bff043fa0394b
DeadVoxelx <deadvoxelx@gmail.com>
2026-07-17 21:11:44 -0400

    Add files via upload

---
+++ Minecraft.World/NurealmBiome.cpp
@@ -0,0 +1,27 @@
+#include "stdafx.h"
+#include "NurealmBiome.h"
+#include "NurealmBiomeDecorator.h"
+#include "net.minecraft.world.entity.monster.h"
+#include "net.minecraft.world.level.tile.h"
+#include "net.minecraft.world.level.levelgen.feature.h"
+
+NurealmBiome::NurealmBiome(int id) : Biome(id)
+{
+	enemies.clear();
+	friendlies.clear();
+	friendlies_chicken.clear();
+	friendlies_wolf.clear();
+	waterFriendlies.clear();
+	ambientFriendlies.clear();
+
+	//enemies.push_back(new MobSpawnerData(eTYPE_NUSKULL, 10, 1, 3));
+	//enemies.push_back(new MobSpawnerData(eTYPE_NUCLEAR_NUSKULL, 1, 1, 2));
+	//enemies.push_back(new MobSpawnerData(eTYPE_NUSA_DEMON, 4, 1, 1));
+
+	decorator = new NurealmBiomeDecorator(this);
+}
+
+Feature *NurealmBiome::getGrassFeature(Random *random)
+{
+	return new TallGrassFeature(Tile::nusaShrub_Id, 1);
+}
---
+++ Minecraft.World/NurealmBiome.h
@@ -0,0 +1,10 @@
+#pragma once
+#include "Biome.h"
+
+class NurealmBiome : public Biome
+{
+public:
+    NurealmBiome(int id);
+	virtual Feature *getGrassFeature(Random *random);
+	//virtual int getSkyColor(float temp);
+};
