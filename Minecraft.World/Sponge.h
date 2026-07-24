0b195b74848057ea9518c93887e150f23941ea79
DeadVoxelx <deadvoxelx@gmail.com>
2026-07-24 09:38:45 -0400

    Update Sponge.h

--- Minecraft.World/Sponge.h
+++ Minecraft.World/Sponge.h
@@ -6,8 +6,9 @@ class Sponge : public Tile
 {
 	friend class Tile;
 public:
-	static const int RANGE = 2;
+	static const int RANGE = 2; // Voxel - this isnt used anymore, but leaving it anyway
 
-protected:
 	Sponge(int id);
+	virtual void onPlace(Level *level, int x, int y, int z);
+	virtual void tick(Level *level, int x, int y, int z, Random *random) override;
 };
