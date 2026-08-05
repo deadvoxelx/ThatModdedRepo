ca651a6cea2968f8d7e17429406933642ae00e02
DeadVoxelx <deadvoxelx@gmail.com>
2026-08-05 04:18:54 -0400

    Update HatchetItem.h

--- Minecraft.World/HatchetItem.h
+++ Minecraft.World/HatchetItem.h
@@ -11,5 +11,6 @@ private:
 public:
 	static void staticCtor();
 	HatchetItem(int id, const Tier *tier);
-	virtual float getDestroySpeed(shared_ptr<ItemInstance> itemInstance, Tile *tile);	// 4J - brought forward from 1.2.3
+	virtual float getDestroySpeed(shared_ptr<ItemInstance> itemInstance, Tile *tile);
+	virtual bool hurtEnemy(shared_ptr<ItemInstance> itemInstance, shared_ptr<LivingEntity> mob, shared_ptr<LivingEntity> attacker);
 };
