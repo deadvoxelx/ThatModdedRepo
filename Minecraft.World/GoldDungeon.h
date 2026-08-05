b6a532fc608e384ff707fc1765109aa7911105e3
DeadVoxelx <deadvoxelx@gmail.com>
2026-08-05 17:11:07 -0400

    Update GoldDungeon.h

--- Minecraft.World/GoldDungeon.h
+++ Minecraft.World/GoldDungeon.h
@@ -8,7 +8,7 @@ class GoldDungeon : public Feature
 public:
 	GoldDungeon(int blockId);
     virtual bool place(Level *level, Random *random, int x, int y, int z);
-	virtual bool generate(Level *level, Random *random, int x, int y, int z, int r);
+	virtual bool generate(Level *level, Random *random, int x, int y, int z, int r, bool spawnBoss = true);
 	
 private:
 	static const int TREASURE_ITEMS_COUNT = 26;
