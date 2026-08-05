48908ba00ee6d0082c1c3640b20772a0803dc8cc
DeadVoxelx <deadvoxelx@gmail.com>
2026-08-05 09:54:43 -0400

    Update GoldIsland.h

--- Minecraft.World/GoldIsland.h
+++ Minecraft.World/GoldIsland.h
@@ -10,10 +10,6 @@ public:
     virtual bool place(Level *level, Random *random, int x, int y, int z);
 	virtual bool generate(Level *level, Random *random, int x, int y, int z, int l);
 	bool generateBlob(Level *level, Random *random, int x, int y, int z, int l);
-	
-private:
-	static const int TREASURE_ITEMS_COUNT = 14;
-	static WeighedTreasure *goldDungeonTreasure[TREASURE_ITEMS_COUNT];
 
 protected:
 	Feature *whiteFlowerFeature;
