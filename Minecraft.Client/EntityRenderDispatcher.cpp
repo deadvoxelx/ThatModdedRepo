41341da7433ffbe116d556ff3b0dd0b9d5f1e510
DeadVoxelx <deadvoxelx@gmail.com>
2026-08-07 09:07:03 -0400

    Update EntityRenderDispatcher.cpp

--- Minecraft.Client/EntityRenderDispatcher.cpp
+++ Minecraft.Client/EntityRenderDispatcher.cpp
@@ -156,7 +156,7 @@ EntityRenderDispatcher::EntityRenderDispatcher()
 
 	renderers[eTYPE_HELLSPHERE] = new ItemSpriteRenderer(Item::hellSphere);
 	//renderers[eTYPE_SLIMEBALL] = new ItemSpriteRenderer(Item::slimeBall);
-	renderers[eTYPE_ZEPHYRBALL] = new FireballRenderer(2.0f);
+	renderers[eTYPE_ZEPHYRBALL] = new ItemSpriteRenderer(Item::snowBall);
 	renderers[eTYPE_DART] = new ArrowRenderer();
 	renderers[eTYPE_DARTENCHANTED] = new ArrowRenderer();
 	//
