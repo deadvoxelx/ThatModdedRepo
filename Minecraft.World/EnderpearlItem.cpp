2f8f8e18aedc4d6072021e2fd9cf4d80e3559f14
DeadVoxelx <deadvoxelx@gmail.com>
2026-08-07 08:53:29 -0400

    Update EnderpearlItem.cpp

--- Minecraft.World/EnderpearlItem.cpp
+++ Minecraft.World/EnderpearlItem.cpp
@@ -17,8 +17,6 @@ bool EnderpearlItem::TestUse(shared_ptr<ItemInstance> itemInstance, Level *level
 
 shared_ptr<ItemInstance> EnderpearlItem::use(shared_ptr<ItemInstance> instance, Level *level, shared_ptr<Player> player)
 {
-	// 4J-PB - Not sure why this was disabled for creative mode, so commenting out
-	//if (player->abilities.instabuild) return instance;
 	if (player->riding != NULL) return instance;
 	if (!player->abilities.instabuild)
 	{
@@ -26,9 +24,7 @@ shared_ptr<ItemInstance> EnderpearlItem::use(shared_ptr<ItemInstance> instance, 
 	}
 
 	level->playEntitySound(player, eSoundType_RANDOM_BOW, 0.5f, 0.4f / (random->nextFloat() * 0.4f + 0.8f));
-	if (!level->isClientSide) 
-	{
-		level->addEntity( shared_ptr<ThrownEnderpearl>( new ThrownEnderpearl(level, player) ) );
-	}
+	player->swing();
+	if (!level->isClientSide) level->addEntity( shared_ptr<ThrownEnderpearl>( new ThrownEnderpearl(level, player) ) );
 	return instance;
 }
