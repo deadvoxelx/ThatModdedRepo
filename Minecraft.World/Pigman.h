f25f37323ecf4855227d3141c0e5df5afb83fcfc
DeadVoxelx <deadvoxelx@gmail.com>
2026-08-08 05:01:04 -0400

    Update Pigman.h

--- Minecraft.World/Pigman.h
+++ Minecraft.World/Pigman.h
@@ -13,29 +13,34 @@ public:
 	eINSTANCEOF GetType() { return eTYPE_PIGMAN; }
 	static Entity *create(Level *level){ return new Pigman(level); }
 
-public:
 	Pigman(Level *level);
 
 	virtual bool useNewAi();
 
+	virtual bool mobInteract(shared_ptr<Player> player);
+
 protected:
 	virtual void registerAttributes();
 	virtual void newServerAiStep();
-
-protected:
 	virtual bool removeWhenFarAway();
 	virtual int getAmbientSound();
 	virtual int getHurtSound();
 	virtual int getDeathSound();
 	virtual void playStepSound(int xt, int yt, int zt, int t);
-
-protected:
 	virtual int getDeathLoot();
 	virtual void dropDeathLoot(bool wasKilledByPlayer, int playerBonusLevel);
 
 public:
 	virtual void thunderHit(const LightningBolt *lightningBolt);
-
-public:
 	virtual shared_ptr<AgableMob> getBreedOffspring(shared_ptr<AgableMob> target);
+
+	virtual void readAdditionalSaveData(CompoundTag *tag);
+
+private:
+	static const int GOLD_INGOT_HOLD_TICKS = 20 * 4;
+	int tradeTimer;
+	void finishTradeOffer();
+
+	static const int TREASURE_ITEMS_COUNT = 13;
+	static WeighedTreasure *pigmanLoot[TREASURE_ITEMS_COUNT];
 };
