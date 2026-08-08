de40fb684b914f7ba14cb9fd7e7d9d95ff3c23fe
DeadVoxelx <deadvoxelx@gmail.com>
2026-08-08 11:47:31 -0400

    Update FurnaceRecipes.cpp

--- Minecraft.World/FurnaceRecipes.cpp
+++ Minecraft.World/FurnaceRecipes.cpp
@@ -57,7 +57,7 @@ FurnaceRecipes::FurnaceRecipes()
 	//Enchanter recipes; only work in the Enchanter
 	addFurnaceRecipy(Tile::quicksoil_Id, new ItemInstance(Tile::glass, 1, 2), .1f);
 	addFurnaceRecipy(Tile::gravititeOre_Id, new ItemInstance(Tile::enchantedGravitite), 1);
-	addFurnaceRecipy(Tile::aercloud_Id, new ItemInstance(Tile::aercloud, 1, 2), .1f);
+	addFurnaceRecipy(Tile::aercloud_Id, new ItemInstance(Tile::aercloud, 1, 1), .1f);
 	addFurnaceRecipy(Tile::holystone_Id, new ItemInstance(Item::healingStone), .35f);
 	addFurnaceRecipy(Item::blueBerry_Id, new ItemInstance(Item::blueBerryEnchanted), .35f);
 	addFurnaceRecipy(Item::dartGold_Id, new ItemInstance(Item::dartEnchanted), .15f);
