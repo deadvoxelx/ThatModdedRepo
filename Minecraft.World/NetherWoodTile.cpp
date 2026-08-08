ba6040a71965ba8a9ad25539295527a3a19ec693
DeadVoxelx <deadvoxelx@gmail.com>
2026-08-08 04:39:43 -0400

    Update NetherWoodTile.cpp

--- Minecraft.World/NetherWoodTile.cpp
+++ Minecraft.World/NetherWoodTile.cpp
@@ -1,6 +1,8 @@
 #include "stdafx.h"
 #include "NetherWoodTile.h"
+#include "TreeTile.h"
 #include "net.minecraft.world.h"
+#include "net.minecraft.world.level.h"
 #include "Facing.h"
 
 NetherWoodTile::NetherWoodTile(int id) : Tile(id, Material::wood)
@@ -18,4 +20,11 @@ void NetherWoodTile::registerIcons(IconRegister *iconRegister)
 {
 	icon = iconRegister->registerIcon(getIconName() + L"_side");
 	iconTop = iconRegister->registerIcon(getIconName() + L"_top");
+}
+
+void NetherWoodTile::playerWillDestroy(Level *level, int x, int y, int z, int data, shared_ptr<Player> player)
+{
+	Tile::playerWillDestroy(level, x, y, z, data, player);
+
+	TreeTile::chopTreeColumn(level, x, y, z, player);
 }
