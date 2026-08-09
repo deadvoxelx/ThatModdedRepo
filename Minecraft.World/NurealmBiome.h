087adac65a465fd56e75d81e208125fc9f66fc9c
DeadVoxelx <deadvoxelx@gmail.com>
2026-08-09 06:16:21 -0400

    Update NurealmBiome.h

--- Minecraft.World/NurealmBiome.h
+++ Minecraft.World/NurealmBiome.h
@@ -5,6 +5,7 @@ class NurealmBiome : public Biome
 {
 public:
     NurealmBiome(int id);
+	virtual Feature *getTreeFeature(Random *random);
 	virtual Feature *getGrassFeature(Random *random);
 	virtual int getSkyColor(float temp);
 };
