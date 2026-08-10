514f271aff1cf1fb32b89e58a9142421f2a806b5
DeadVoxelx <deadvoxelx@gmail.com>
2026-08-10 06:42:43 -0400

    Update Inventory.h

--- Minecraft.World/Inventory.h
+++ Minecraft.World/Inventory.h
@@ -6,6 +6,7 @@ using namespace std;
 
 class Player;
 class CompoundTag;
+class Level;
 
 class Inventory : public Container
 {
@@ -23,7 +24,7 @@ public:
 	ItemInstanceArray aether;
 
 	int selected;
-	Player *player; // This is owned by shared_ptrs, but we are owned by it
+	Player *player;
 
 private:
 	shared_ptr<ItemInstance> heldItem;
@@ -36,17 +37,11 @@ public:
 	~Inventory();
 
 	shared_ptr<ItemInstance> getSelected();
-	// 4J-PB - Added for the in-game tooltips
 	bool IsHeldItem();
 	static int getSelectionSize();
-
-//private:
 	int getSlot(int tileId);
 	int getSlot(int tileId, int data);
-
 	int getSlotWithRemainingSpace(shared_ptr<ItemInstance> item);
-
-//public:
 	int getFreeSlot();
 	void grabTexture(int id, int data, bool checkData, bool mayReplace);
 	void swapPaint(int wheel);
@@ -60,11 +55,9 @@ public:
 	void tick();
 	bool removeResource(int type);
 
-	// 4J-PB added to get the right resource from the inventory for removal
 	bool removeResource(int type,int iAuxVal);
-	void removeResources(shared_ptr<ItemInstance> item); // 4J Added for trading
+	void removeResources(shared_ptr<ItemInstance> item);
 
-	// 4J-Stu added to the get the item that would be affected by the removeResource functions
 	shared_ptr<ItemInstance> getResourceItem(int type);
 	shared_ptr<ItemInstance> getResourceItem(int type,int iAuxVal);
 
@@ -77,7 +70,7 @@ public:
 	void setItem(unsigned int slot, shared_ptr<ItemInstance> item);
 	float getDestroySpeed(Tile *tile);
 	ListTag<CompoundTag> *save(ListTag<CompoundTag> *listTag);
-	void load(ListTag<CompoundTag> *inventoryList);
+	void load(ListTag<CompoundTag> *inventoryList, Level *level);
 	unsigned int getContainerSize();
 	shared_ptr<ItemInstance> getItem(unsigned int slot);
 	wstring getName();
@@ -106,5 +99,5 @@ public:
 	bool canPlaceItem(int slot, shared_ptr<ItemInstance> item);
 	void replaceWith(shared_ptr<Inventory> other);
 
-	int countMatches(shared_ptr<ItemInstance> itemInstance); // 4J Added
+	int countMatches(shared_ptr<ItemInstance> itemInstance);
 };
