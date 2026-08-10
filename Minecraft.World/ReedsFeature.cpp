0ae2c8edf73f5294e3ec20ea028b2e9bf6cf2021
DeadVoxelx <deadvoxelx@gmail.com>
2026-08-10 15:18:37 -0400

    Update ReedsFeature.cpp

--- Minecraft.World/ReedsFeature.cpp
+++ Minecraft.World/ReedsFeature.cpp
@@ -1,6 +1,7 @@
 #include "stdafx.h"
 #include "net.minecraft.world.level.h"
 #include "net.minecraft.world.level.tile.h"
+#include "net.minecraft.world.level.dimension.h"
 #include "ReedsFeature.h"
 
 bool ReedsFeature::place(Level *level, Random *random, int x, int y, int z)
@@ -11,36 +12,57 @@ bool ReedsFeature::place(Level *level, Random *random, int x, int y, int z)
 		int y2 = y;
 		int z2 = z + random->nextInt(4) - random->nextInt(4);
 
-		// 4J Stu Added to stop reed features generating areas previously place by game rule generation
 		if(app.getLevelGenerationOptions() != nullptr)
 		{
 			LevelGenerationOptions *levelGenOptions = app.getLevelGenerationOptions();
 			bool intersects = levelGenOptions->checkIntersects(x2, y2, z2, x2, y2, z2);
 			if(intersects)
 			{
-				//app.DebugPrintf("Skipping reeds feature generation as it overlaps a game rule structure\n");
 				continue;
 			}
 		}
-		if (level->isEmptyTile(x2, y2, z2)) 
+		if (level->dimension->id == -1)
 		{
-			if (level->getMaterial(x2-1, y2-1, z2) == Material::water || 
-				level->getMaterial(x2+1, y2-1, z2) == Material::water || 
-				level->getMaterial(x2, y2-1, z2-1) == Material::water || 
-				level->getMaterial(x2, y2-1, z2+1) == Material::water)
+			if (level->isEmptyTile(x2, y2, z2)) 
 			{
-
-				int h = 2 + random->nextInt(random->nextInt(3) + 1);
-				for (int yy = 0; yy < h; yy++)
+				if (level->getMaterial(x2-1, y2-1, z2) == Material::lava || 
+					level->getMaterial(x2+1, y2-1, z2) == Material::lava || 
+					level->getMaterial(x2, y2-1, z2-1) == Material::lava || 
+					level->getMaterial(x2, y2-1, z2+1) == Material::lava)
 				{
-					if ( Tile::reeds->canSurvive(level, x2, y2 + yy, z2) )
+
+					int h = 2 + random->nextInt(4);
+					for (int yy = 0; yy < h; yy++)
 					{
-						level->setTileAndData(x2, y2 + yy, z2, Tile::reeds_Id, 0, Tile::UPDATE_CLIENTS);
+						if ( Tile::lavaReed->canSurvive(level, x2, y2 + yy, z2) )
+						{
+							level->setTileAndData(x2, y2 + yy, z2, Tile::lavaReed_Id, 0, Tile::UPDATE_CLIENTS);
+						}
+					}
+				}
+			}
+		}
+		else
+		{
+			if (level->isEmptyTile(x2, y2, z2)) 
+			{
+				if (level->getMaterial(x2-1, y2-1, z2) == Material::water || 
+					level->getMaterial(x2+1, y2-1, z2) == Material::water || 
+					level->getMaterial(x2, y2-1, z2-1) == Material::water || 
+					level->getMaterial(x2, y2-1, z2+1) == Material::water)
+				{
+
+					int h = 2 + random->nextInt(random->nextInt(3) + 1);
+					for (int yy = 0; yy < h; yy++)
+					{
+						if ( Tile::reeds->canSurvive(level, x2, y2 + yy, z2) )
+						{
+							level->setTileAndData(x2, y2 + yy, z2, Tile::reeds_Id, 0, Tile::UPDATE_CLIENTS);
+						}
 					}
 				}
 			}
 		}
 	}
-
 	return true;
 }
