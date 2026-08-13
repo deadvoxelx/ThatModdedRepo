6ff74b8bbf052dd044fd012922f4c14e54baac10
DeadVoxelx <deadvoxelx@gmail.com>
2026-08-13 14:13:32 -0400

    Update ArrowInfiniteEnchantment.cpp

--- Minecraft.World/ArrowInfiniteEnchantment.cpp
+++ Minecraft.World/ArrowInfiniteEnchantment.cpp
@@ -1,4 +1,9 @@
 #include "stdafx.h"
+#include "net.minecraft.world.item.h"
+#include "DartShooterGoldItem.h"
+#include "DartShooterPoisonItem.h"
+#include "DartShooterEnchantedItem.h"
+#include "DartShooterNethaniumItem.h"
 #include "ArrowInfiniteEnchantment.h"
 
 ArrowInfiniteEnchantment::ArrowInfiniteEnchantment(int id, int frequency) : Enchantment(id, frequency, EnchantmentCategory::bow)
@@ -8,15 +13,23 @@ ArrowInfiniteEnchantment::ArrowInfiniteEnchantment(int id, int frequency) : Ench
 
 int ArrowInfiniteEnchantment::getMinCost(int level)
 {
-	return 20;
+	return 5;
 }
 
 int ArrowInfiniteEnchantment::getMaxCost(int level)
 {
-	return 50;
+	return 15;
 }
 
 int ArrowInfiniteEnchantment::getMaxLevel()
 {
 	return 1;
+}
+
+bool ArrowInfiniteEnchantment::canEnchant(shared_ptr<ItemInstance> item)
+{
+	if (dynamic_cast<DartShooterGoldItem *>(item->getItem()) != nullptr) return true;
+	if (dynamic_cast<DartShooterEnchantedItem *>(item->getItem()) != nullptr) return true;
+	if (dynamic_cast<DartShooterNethaniumItem *>(item->getItem()) != nullptr) return true;
+	return Enchantment::canEnchant(item);
 }
