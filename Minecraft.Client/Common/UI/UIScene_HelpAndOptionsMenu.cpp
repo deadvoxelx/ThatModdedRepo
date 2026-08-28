ec18185370718300114bcb8ecf4f3f9afdd08fcf
DeadVoxelx <deadvoxelx@gmail.com>
2026-08-28 09:37:05 -0400

    Update UIScene_HelpAndOptionsMenu.cpp

--- Minecraft.Client/Common/UI/UIScene_HelpAndOptionsMenu.cpp
+++ Minecraft.Client/Common/UI/UIScene_HelpAndOptionsMenu.cpp
@@ -3,27 +3,36 @@
 #include "Screen.h"
 #include "OptionsScreen.h"
 #include "VideoSettingsScreen.h"
+#include "ControlsScreen.h"
+#include "DirtBackgroundScreen.h"
 #include "UIScene_HelpAndOptionsMenu.h"
 #include "..\..\Minecraft.h"
+#include <fstream>
+#include <windows.h>
+#include <shellapi.h>
 
 UIScene_HelpAndOptionsMenu::UIScene_HelpAndOptionsMenu(int iPad, void *initData, UILayer *parentLayer) : UIScene(iPad, parentLayer)
 {
 	// Setup all the Iggy references we need for this scene
 	initialiseMovie();
-	
+
 	m_bNotInGame=(Minecraft::GetInstance()->level==nullptr);
 
-	m_buttons[BUTTON_HAO_HOWTOPLAY].init(IDS_HOW_TO_PLAY,BUTTON_HAO_HOWTOPLAY);
+	m_buttons[BUTTON_HAO_WIKI].init(IDS_WIKI,BUTTON_HAO_WIKI);
 	m_buttons[BUTTON_HAO_SETTINGS].init(IDS_SETTINGS,BUTTON_HAO_SETTINGS);
 	m_buttons[BUTTON_HAO_AUDIO].init(IDS_AUDIO,BUTTON_HAO_AUDIO);
 	m_buttons[BUTTON_HAO_GRAPHICS].init(IDS_GRAPHICS,BUTTON_HAO_GRAPHICS);
 	m_buttons[BUTTON_HAO_UI].init(IDS_USER_INTERFACE,BUTTON_HAO_UI);
 	m_buttons[BUTTON_HAO_SENSITIVITY].init(IDS_SENSITIVITY,BUTTON_HAO_SENSITIVITY);
 	m_buttons[BUTTON_HAO_CONTROLS].init(IDS_CONTROLS,BUTTON_HAO_CONTROLS);
-	m_buttons[BUTTON_HAO_CREDITS].init(IDS_CREDITS,BUTTON_HAO_CREDITS);
-	
-	/* 4J-TomK - we should never remove a control before the other buttons controls are initialised!
-	(because vita touchboxes are rebuilt on remove since the remaining positions might change) */
+	if (Minecraft::GetInstance()->level != nullptr)
+	{
+		m_buttons[BUTTON_HAO_CREDITS].init(IDS_CHANGE_SKIN,BUTTON_HAO_CREDITS);
+	}
+	else
+	{
+		m_buttons[BUTTON_HAO_CREDITS].init(IDS_CREDITS,BUTTON_HAO_CREDITS);
+	}
 	
 	if(app.GetLocalPlayerCount()>1)
 	{
@@ -68,11 +77,10 @@ void UIScene_HelpAndOptionsMenu::updateComponents()
 	else
 	{
 		m_parentLayer->showComponent(m_iPad,eUIComponent_Panorama,false);
-		m_parentLayer->showComponent(m_iPad,eUIComponent_MenuBackground,false);
+		m_parentLayer->showComponent(m_iPad,eUIComponent_MenuBackground,true);
 
 		if( app.GetLocalPlayerCount() == 1 ) m_parentLayer->showComponent(m_iPad,eUIComponent_Logo,true);
 		else m_parentLayer->showComponent(m_iPad,eUIComponent_Logo,false);
-
 	}
 }
 
@@ -120,8 +128,9 @@ void UIScene_HelpAndOptionsMenu::handlePress(F64 controlId, F64 childId)
 {
 	switch(static_cast<int>(controlId))
 	{
-	case BUTTON_HAO_HOWTOPLAY:
-		ui.NavigateToScene(m_iPad, eUIScene_HowToPlayMenu);
+	case BUTTON_HAO_WIKI:
+		//ui.NavigateToScene(m_iPad, eUIScene_HowToPlayMenu);
+		ShellExecute(0, 0, "https://hellishends.miraheze.org/wiki/Main_Page", 0, 0 , SW_SHOW );
 		break;
 	case BUTTON_HAO_SETTINGS:
 		ui.NavigateToScene(m_iPad, eUIScene_SettingsOptionsMenu);
@@ -139,12 +148,25 @@ void UIScene_HelpAndOptionsMenu::handlePress(F64 controlId, F64 childId)
 		ui.NavigateToScene(m_iPad, eUIScene_SettingsControlMenu);
 		break;
 	case BUTTON_HAO_CONTROLS:
+#ifdef _WINDOWS64
+		ui.CloseAllPlayersScenes();
+		m_parentLayer->removeComponent(eUIComponent_Panorama);
+		m_parentLayer->removeComponent(eUIComponent_Logo);
+		m_parentLayer->removeComponent(eUIComponent_MenuBackground);
+		Minecraft::GetInstance()->setScreen(new ControlsScreen(nullptr, Minecraft::GetInstance()->options));
+#else
 		ui.NavigateToScene(m_iPad, eUIScene_ControlsMenu);
+#endif
 		break;
 	case BUTTON_HAO_CREDITS:
-		ui.NavigateToScene(m_iPad, eUIScene_Credits);
-		//minecraft->setScreen(new OptionsScreen(lastScreen, minecraft->options));
-		//screen->renderDirtBackground(0);
+		if(Minecraft::GetInstance()->level != nullptr)
+		{
+			ui.NavigateToScene(m_iPad, eUIScene_SkinSelectMenu);
+		}
+		else
+		{
+			ui.NavigateToScene(m_iPad, eUIScene_Credits);
+		}
 		break;
 	}
 }
