9a43797b9e644cd31da99d661c296d1f180b47b9
DeadVoxelx <deadvoxelx@gmail.com>
2026-08-28 09:43:05 -0400

    Update ControlsScreen.cpp

--- Minecraft.Client/ControlsScreen.cpp
+++ Minecraft.Client/ControlsScreen.cpp
@@ -3,11 +3,16 @@
 #include "Options.h"
 #include "SmallButton.h"
 #include "..\Minecraft.World\net.minecraft.locale.h"
+#ifdef _WINDOWS64
+#include "Windows64\KeyboardMouseInput.h"
+#endif
+
+extern ConsoleUIController ui;
 
 ControlsScreen::ControlsScreen(Screen *lastScreen, Options *options)
 {
 	// 4J - added initialisers
-	title == L"Controls";
+	title = L"Controls";
 	selectedKey = -1;
 
 	this->lastScreen = lastScreen;
@@ -26,12 +31,10 @@ void ControlsScreen::init()
     int leftPos = getLeftScreenPosition();
     for (int i = 0; i < Options::keyMappings_length; i++)
 	{
-        buttons.push_back(new SmallButton(i, leftPos + i % 2 * ROW_WIDTH, height / 6 + 24 * (i >> 1), BUTTON_WIDTH, 20, options->getKeyMessage(i)));
+        buttons.push_back(new SmallButton(i, leftPos + i % 3 * ROW_WIDTH / 1.5 + 15, height / 12 + ROW_HEIGHT * (i / 3), BUTTON_WIDTH, 20, options->getKeyMessage(i)));
     }
 
-    buttons.push_back(new Button(200, width / 2 - 100, height / 6 + 24 * 7, language->getElement(L"gui.done")));
-    title = language->getElement(L"controls.title");
-
+    buttons.push_back(new Button(200, width / 2 - 100, height / 6 + 24 * 7, app.GetString(IDS_DONE)));
 }
 
 void ControlsScreen::buttonClicked(Button *button)
@@ -42,12 +45,15 @@ void ControlsScreen::buttonClicked(Button *button)
     }
     if (button->id == 200)
 	{
-        minecraft->setScreen(lastScreen);
+        //minecraft->setScreen(lastScreen);
+        ui.PlayUISFX(eSFX_Back);
+        ui.NavigateToScene(ProfileManager.GetPrimaryPad(), eUIScene_HelpAndOptionsMenu);
     }
 	else
 	{
         selectedKey = button->id;
         button->msg = L"> " + options->getKeyMessage(button->id) + L" <";
+        ui.PlayUISFX(eSFX_Press);
     }
 }
 
@@ -55,9 +61,7 @@ void ControlsScreen::keyPressed(wchar_t eventCharacter, int eventKey)
 {
     if (selectedKey >= 0)
 	{
-        options->setKey(selectedKey, eventKey);
-        buttons[selectedKey]->msg = options->getKeyMessage(selectedKey);
-        selectedKey = -1;
+        return;
     }
 	else
 	{
@@ -65,15 +69,37 @@ void ControlsScreen::keyPressed(wchar_t eventCharacter, int eventKey)
     }
 }
 
+void ControlsScreen::tick()
+{
+    if (selectedKey >= 0)
+	{
+#ifdef _WINDOWS64
+        int vk = g_KBMInput.GetPressedKey();
+        if (vk == VK_ESCAPE)
+        {
+            buttons[selectedKey]->msg = options->getKeyMessage(selectedKey);
+            selectedKey = -1;
+        }
+        else if (vk != 0)
+        {
+            options->setKey(selectedKey, vk);
+            buttons[selectedKey]->msg = options->getKeyMessage(selectedKey);
+            selectedKey = -1;
+        }
+#endif
+    }
+    Screen::tick();
+}
+
 void ControlsScreen::render(int xm, int ym, float a)
 {
     renderBackground();
-    drawCenteredString(font, title, width / 2, 20, 0xffffff);
+    renderDirtBackground(0);
 
     int leftPos = getLeftScreenPosition();
     for (int i = 0; i < Options::keyMappings_length; i++)
 	{
-        drawString(font, options->getKeyDescription(i), leftPos + i % 2 * ROW_WIDTH + BUTTON_WIDTH + 6, height / 6 + 24 * (i >> 1) + 7, 0xffffffff);
+        drawString(font, options->getKeyDescription(i), leftPos + i % 3 * ROW_WIDTH / 1.5 + 15, height / 12 + ROW_HEIGHT * (i / 3) - 10, 0xffffffff);
     }
 
     Screen::render(xm, ym, a);
