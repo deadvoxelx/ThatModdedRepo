f6eeb8c19921ebd0c72ae836768dbcdfa7aa3290
DeadVoxelx <deadvoxelx@gmail.com>
2026-08-28 09:42:35 -0400

    Update ControlsScreen.h

--- Minecraft.Client/ControlsScreen.h
+++ Minecraft.Client/ControlsScreen.h
@@ -7,8 +7,10 @@ class ControlsScreen : public Screen
 {
 private:
 	Screen *lastScreen;
+
 protected:
 	wstring title;
+
 private:
 	Options *options;
     
@@ -16,16 +18,22 @@ private:
 
     static const int BUTTON_WIDTH = 70;
     static const int ROW_WIDTH = 160;
+    static const int ROW_HEIGHT = 32;
 
 public:
 	ControlsScreen(Screen *lastScreen, Options *options);
+
 private:
 	int getLeftScreenPosition();
+
 public:
 	void init();
+
 protected:
 	void buttonClicked(Button *button);
     void keyPressed(wchar_t eventCharacter, int eventKey);
+
 public:
+	void tick();
 	void render(int xm, int ym, float a);
 };
