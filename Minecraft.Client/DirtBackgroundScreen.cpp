a82f160d69a4604e8d5e937d2f44db6fe650c170
DeadVoxelx <deadvoxelx@gmail.com>
2026-08-28 10:30:08 -0400

    Add DirtBackgroundScreen.cpp & .h

---
+++ Minecraft.Client/DirtBackgroundScreen.cpp
@@ -0,0 +1,14 @@
+#include "DirtBackgroundScreen.h"
+
+DirtBackgroundScreen::DirtBackgroundScreen()
+{
+}
+
+void DirtBackgroundScreen::render(int xm, int ym, float a)
+{
+	renderDirtBackground(0);
+}
+
+void DirtBackgroundScreen::keyPressed(wchar_t eventCharacter, int eventKey)
+{
+}
